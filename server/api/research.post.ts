cd7557caae96b58449cb5eaa921a3fc87eeacd93
mikechao <mike.chao.one@gmail.com>
2025-02-18 12:27:41 -0800

    Added consola and basic debug message

--- server/api/research.post.ts
+++ server/api/research.post.ts
@@ -1,7 +1,9 @@
+import { consola } from 'consola'
+
 export default defineLazyEventHandler(async () => {
   return defineEventHandler(async (webEvent) => {
     const body = await readBody(webEvent)
-
+    consola.debug({ tag: 'eventHandler', message: `Got ${JSON.stringify(body)}` })
     return 'Hello from research.post!'
   })
 })
