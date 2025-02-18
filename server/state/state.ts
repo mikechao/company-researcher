24ae4a2bf49e89b113c810b9f3ff7c9ece70fab2
mikechao <mike.chao.one@gmail.com>
2025-02-18 13:44:33 -0800

    Added InputState and OutputState

--- server/state/state.ts
+++ server/state/state.ts
@@ -97,3 +97,89 @@ export const OverallState = Annotation.Root({
     default: () => 0,
   }),
 })
+
+/**
+ * Input state defines the interface between the graph and the user (external API).
+ */
+export const InputState = Annotation.Root({
+  /**
+   * Company to research provided by the user.
+   */
+  company: Annotation<string>(),
+  /**
+   * The json schema defines the information the agent is tasked with filling out.
+   */
+  extractionSchema: Annotation<Record<string, any>>({
+    reducer: (_state: Record<string, any>, update: Record<string, any>) => update,
+    default: () => defaultExtractionSchema,
+  }),
+  /**
+   * Any notes from the user to start the research process.
+   */
+  userNotes: Annotation<string>(),
+})
+
+export const OutputState = Annotation.Root({
+  /**
+   * Company to research provided by the user.
+   */
+  company: Annotation<string>(),
+  /**
+   * The json schema defines the information the agent is tasked with filling out.
+   */
+  extractionSchema: Annotation<Record<string, any>>({
+    reducer: (_state: Record<string, any>, update: Record<string, any>) => update,
+    default: () => defaultExtractionSchema,
+  }),
+  /**
+   * Any notes from the user to start the research process.
+   */
+  userNotes: Annotation<string>(),
+  /**
+   * List of generated search queries to find relevant information
+   */
+  searchQueries: Annotation<string[]>({
+    reducer: (state: string[], update: string[]) => {
+      state.push(...update)
+      return state
+    },
+    default: () => [],
+  }),
+  /**
+   * List of search results
+   */
+  searchResult: Annotation<Record<string, any>[]>({
+    reducer: (state: Record<string, any>[], update: Record<string, any>[]) => {
+      state.push(...update)
+      return state
+    },
+    default: () => [],
+  }),
+  /**
+   * Notes from completed research related to the schema
+   */
+  completedNotes: Annotation<string[]>({
+    reducer: (state: string[], update: string[]) => {
+      state.push(...update)
+      return state
+    },
+    default: () => [],
+  }),
+  /**
+   * A dictionary containing the extracted and processed information
+   * based on the user's query and the graph's execution.
+   * This is the primary output of the enrichment process.
+   */
+  info: Annotation<Record<string, any>>(),
+  /**
+   * True if all required fields are well populated, False otherwise
+   */
+  isSatisfactory: Annotation<boolean>(),
+  /**
+   * Number of times the reflection node has been executed
+   */
+  reflectionStepsTaken: Annotation<number>({
+    reducer: (_state: number, update: number) => update,
+    default: () => 0,
+  }),
+})
