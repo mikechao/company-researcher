1a5a8e145721cd44a47658f8d1f7ba28754102b3
mikechao <mike.chao.one@gmail.com>
2025-02-18 11:22:45 -0800

    Setup api keys needed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
+import process from 'node:process'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    anthropicAPIKey: process.env.NUXT_ANTHROPIC_API_KEY,
+    tavilyAPIKey: process.env.NUXT_TAVILY_API_KEY,
+  },
 })
