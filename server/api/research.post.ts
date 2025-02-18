5a32601d9852876a838ac08a39a58ab419484fe8
mikechao <mike.chao.one@gmail.com>
2025-02-18 12:39:53 -0800

    Setup simple invocation of claude 3.5 sonnet

--- server/api/research.post.ts
+++ server/api/research.post.ts
@@ -1,9 +1,24 @@
+import { ChatAnthropic } from '@langchain/anthropic'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 import { consola } from 'consola'
 
 export default defineLazyEventHandler(async () => {
+  const runtimeConfig = useRuntimeConfig()
+
+  const model = new ChatAnthropic({
+    anthropicApiKey: runtimeConfig.anthropicAPIKey,
+    model: 'claude-3-5-sonnet-latest',
+  })
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', 'Give a short introduction of yourself to the user'],
+    ['human', 'Tell me a joke about {topic}'],
+  ])
+
   return defineEventHandler(async (webEvent) => {
     const body = await readBody(webEvent)
     consola.debug({ tag: 'eventHandler', message: `Got ${JSON.stringify(body)}` })
-    return 'Hello from research.post!'
+    const result = await prompt.pipe(model).invoke({ topic: 'cats' })
+    return result.content
   })
 })
