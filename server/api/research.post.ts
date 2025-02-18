6ed599b9d5df863adec4ff7c86fae839c668e7ce
mikechao <mike.chao.one@gmail.com>
2025-02-18 14:26:58 -0800

    Porting generateQueries node

--- server/api/research.post.ts
+++ server/api/research.post.ts
@@ -1,6 +1,11 @@
+import type { RunnableConfig } from '@langchain/core/runnables'
+import type { ConfigurableAnnotation } from '../state/configuration'
+import type { OverallState } from '../state/state'
 import { ChatAnthropic } from '@langchain/anthropic'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { ChatPromptTemplate, PromptTemplate } from '@langchain/core/prompts'
 import { consola } from 'consola'
+import { z } from 'zod'
+import { QUERY_WRITER_PROMPT } from '../prompts/prompts'
 
 export default defineLazyEventHandler(async () => {
   const runtimeConfig = useRuntimeConfig()
@@ -14,6 +19,35 @@ export default defineLazyEventHandler(async () => {
     ['system', 'Give a short introduction of yourself to the user'],
     ['human', 'Tell me a joke about {topic}'],
   ])
+
+  const generateQueries = async (
+    state: typeof OverallState.State,
+    config: RunnableConfig<typeof ConfigurableAnnotation.State>,
+  ) => {
+    const maxSearchQueries = config.configurable?.maxSearchQueries
+
+    const querySchema = z.object({
+      queries: z.array(z.string()).describe('List of search queries.'),
+    })
+
+    const structuredModel = model.withStructuredOutput(querySchema)
+    const queryInstructions = await PromptTemplate.fromTemplate(QUERY_WRITER_PROMPT)
+      .format({
+        company: state.company,
+        max_search_queries: maxSearchQueries,
+        info: JSON.stringify(state.extractionSchema, null, 2),
+        user_notes: state.userNotes,
+      })
+
+    const results = await structuredModel.invoke([
+      {"role": "system", "content": queryInstructions},
+      {
+        "role": "user",
+        "content": "Please generate a list of search queries related to the schema that you want to populate.",
+      }
+    ])
+    return {searchQueries: results.queries}
+  }
 
   return defineEventHandler(async (webEvent) => {
     const body = await readBody(webEvent)
