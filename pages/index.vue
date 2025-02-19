4207968eaa2b2a3018b925fdabd561581bfe3564
mikechao <mike.chao.one@gmail.com>
2025-02-19 15:25:51 -0800

    Setup basic ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <h1>Basic index</h1>
-  </div>
+  <UCard class="justify-center h-screen">
+    <div class="flex flex-col items-center">
+      <div class="flex items-center">
+        <label for="company-name" class="mr-2">Company Name</label>
+        <UInput
+        v-model="companyName"
+        label="Company Name"
+        placeholder="Enter company name"
+        color="primary"
+        variant="outline"
+        id="company-name"
+        />
+      </div>
+      <UButton
+        label="Research"
+        icon="i-mdi-microscope"
+        :trailing="true"
+        color="primary"
+        class="mt-4"
+      />
+    </div>
+  </UCard>
 </template>
+
+<script setup lang="ts">
+const companyName = ref('')
+</script>
