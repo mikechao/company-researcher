90cc277b27bcbcd5d3c1468f1298802ca8c0d152
mikechao <mike.chao.one@gmail.com>
2025-02-19 12:15:54 -0800

    Added constant for event names

---
+++ types/constants.ts
@@ -0,0 +1,7 @@
+export const EVENT_NAMES = Object.freeze({
+  GENERATE_QUERIES: 'generateQueries',
+  RESEARCH_COMPANY: 'researchCompany',
+  GATHER_NOTES: 'gatherNotes',
+  REFLECTION: 'reflection',
+  END: 'END',
+} as const)
