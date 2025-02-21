d8a0f986a104437e76455e218b0e865fca224f02
mikechao <mike.chao.one@gmail.com>
2025-02-21 10:41:57 -0800

    Use a UCard to wrap ResearchResult

--- components/ResearchResults.vue
+++ components/ResearchResults.vue
@@ -34,28 +34,32 @@ function formatKey(key) {
 </script>
 
 <template>
-  <div class="shadow-sm bg-transparent text-gray-900 dark:text-white rounded-md ring-1 ring-inset ring-primary-500 dark:ring-primary-400 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400">
+  <UCard
+    class="p-4" :ui="{
+      ring: 'ring-1 ring-inset ring-primary-500 dark:ring-primary-400 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400',
+    }"
+  >
     <!-- If the data is an object -->
     <template v-if="isObject(data)">
       <div v-for="(value, key) in data" :key="key" class="mb-2 pl-4 ">
-        <span class="font-semibold text-gray-700 mr-2">{{ formatKey(key) }}:</span>
+        <span class="font-semibold text-gray-700 dark:text-gray-300 mr-2">{{ formatKey(key) }}:</span>
         <template v-if="isObject(value)">
           <!-- Recursively render nested objects -->
           <ResearchResults :data="value" />
         </template>
         <template v-else-if="isArray(value)">
           <!-- For arrays, join the values or render each item recursively -->
-          <span class="text-gray-600">{{ value.join(', ') }}</span>
+          <span class="text-gray-600 dark:text-gray-400">{{ value.join(', ') }}</span>
         </template>
         <template v-else>
-          <span class="text-gray-600">{{ value }}</span>
+          <span class="text-gray-600 dark:text-gray-400">{{ value }}</span>
         </template>
       </div>
     </template>
 
     <!-- If the data is an array -->
     <template v-else-if="isArray(data)">
-      <div v-for="(item, index) in data" :key="index" class="mb-2 pl-4 border-l-2 border-gray-200">
+      <div v-for="(item, index) in data" :key="index" class="mb-2 pl-4 ">
         <ResearchResults :data="item" />
       </div>
     </template>
@@ -64,5 +68,5 @@ function formatKey(key) {
     <template v-else>
       <span class="text-gray-600">{{ data }}</span>
     </template>
-  </div>
+  </UCard>
 </template>
