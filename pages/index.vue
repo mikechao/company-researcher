f21105b139725f9afb5f452d23dc56c1f442f3ee
mikechao <mike.chao.one@gmail.com>
2025-02-22 10:50:43 -0800

    Add a transition when ResearchParamHelp is shown

--- pages/index.vue
+++ pages/index.vue
@@ -315,7 +315,19 @@ const schema = z.object({
               class="mt-4"
             />
           </div>
-          <ResearchParamHelp v-if="hoveredField" :param-name="hoveredField" />
+          <Transition
+            enter-active-class="transition duration-300 ease-out"
+            enter-from-class="transform -translate-y-2 opacity-0"
+            enter-to-class="transform translate-y-0 opacity-100"
+            leave-active-class="transition duration-200 ease-in"
+            leave-from-class="transform translate-y-0 opacity-100"
+            leave-to-class="transform -translate-y-2 opacity-0"
+          >
+            <ResearchParamHelp
+              v-if="hoveredField"
+              :param-name="hoveredField"
+            />
+          </Transition>
         </div>
       </div>
     </Transition>
