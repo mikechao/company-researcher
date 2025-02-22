9ab86d3e05fb06bc1affe2af049e684ea1a98f2e
mikechao <mike.chao.one@gmail.com>
2025-02-22 10:57:08 -0800

    Move color mode to own component

---
+++ components/ColorModeButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+interface ColorModeLabel {
+  label: string
+  icon: string
+}
+
+const darkMode: ColorModeLabel = {
+  label: 'Dark',
+  icon: 'i-mdi-weather-night',
+}
+
+const lightMode: ColorModeLabel = {
+  label: 'Light',
+  icon: 'i-mdi-weather-sunny',
+}
+
+const selectedMode = computed<ColorModeLabel>(() =>
+  colorMode.value === 'dark' ? darkMode : lightMode,
+)
+
+function changeColorMode() {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <UButton
+    :label="selectedMode.label"
+    color="gray"
+    variant="ghost"
+    :icon="selectedMode.icon"
+    @click="changeColorMode"
+  />
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,47 +1,23 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+import ColorModeButton from '~/components/ColorModeButton.vue'
 
-interface ColorModeLabel {
-  label: string
-  icon: string
-}
-
-const darkMode: ColorModeLabel = {
-  label: 'Dark',
-  icon: 'i-mdi-weather-night',
-}
-
-const lightMode: ColorModeLabel = {
-  label: 'Light',
-  icon: 'i-mdi-weather-sunny',
-}
-
-const selectedMode = computed<ColorModeLabel>(() =>
-  colorMode.value === 'dark' ? darkMode : lightMode,
-)
-
-function changeColorMode() {
-  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-}
-
-const links = computed(() => [
-  [{
+const links = [
+  {
     label: 'Research',
     icon: 'i-mdi-microscope',
     to: '/',
-  }],
-  [{
-    label: selectedMode.value.label,
-    icon: selectedMode.value.icon,
-    click: changeColorMode,
-
-  }],
-])
+  },
+]
 </script>
 
 <template>
   <div class="flex flex-col h-screen overflow-hidden">
-    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+    <div class="flex items-center justify-between border-b border-gray-200 dark:border-gray-800">
+      <UHorizontalNavigation :links="links" />
+      <ColorScheme placeholder="..." tag="span" class="mr-2">
+        <ColorModeButton class="mr-2" />
+      </ColorScheme>
+    </div>
     <div class="flex-1 overflow-auto">
       <slot />
     </div>
