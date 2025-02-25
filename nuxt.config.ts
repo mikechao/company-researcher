00ed5d001d4e74d7165bb57dfac68d1b0c15749b
mikechao <mike.chao.one@gmail.com>
2025-02-24 17:04:33 -0800

    Remove nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/app.css'],
   modules: [
-    '@nuxt/ui',
+    '@nuxt/ui-pro',
     '@vueuse/nuxt',
     '@nuxtjs/color-mode',
     'nuxt-codemirror',
