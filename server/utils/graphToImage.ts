835b25f6adca65be9b3536125d8a488ddb2dd888
mikechao <mike.chao.one@gmail.com>
2025-02-25 13:20:32 -0800

    Added util to turn the graph into image

---
+++ server/utils/graphToImage.ts
@@ -0,0 +1,28 @@
+import { Buffer } from 'node:buffer'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import consola from 'consola'
+
+export async function graphToImage(graph: any, outputPath?: string) {
+  const drawableGraph = await graph.getGraphAsync()
+  const image = await drawableGraph.drawMermaidPng()
+  const arrayBuffer = await image.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+
+  const filePath = outputPath || path.join(defaultPath(), 'graph.png')
+
+  try {
+    await fs.writeFile(filePath, buffer)
+    consola.success({ tag: 'eventHandler', message: `Graph image saved to ${filePath}` })
+  }
+  catch (error) {
+    consola.error({ tag: 'eventHandler', message: `Error saving graph image: ${error}` })
+  }
+}
+
+function defaultPath() {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  return __dirname
+}
