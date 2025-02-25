ed545d540003df7e775a4af0835dfedfcd41d419
mikechao <mike.chao.one@gmail.com>
2025-02-24 16:02:41 -0800

    Basic landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="flex flex-col h-screen overflow-hidden">
-    <h3>Welcome</h3>
+  <div class="flex flex-col h-full w-full overflow-hidden">
+    <div class="flex flex-col items-center justify-center mt-2 space-y-2">
+      <h1>
+        <span class="text-5xl max-sm:text-2xl font-bold">
+          Company Researcher Agent
+        </span>
+      </h1>
+      <h2>
+        <span class="text-2xl max-sm:text-lg">
+          An AI agent that scans the web for details about a company provided by the user.
+        </span>
+      </h2>
+      <h2>
+        <span class="text-2xl max-sm:text-lg">
+          A report will be generated based on a JSON schema specified by the user.
+        </span>
+      </h2>
+    </div>
   </div>
 </template>
