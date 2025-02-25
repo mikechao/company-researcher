a34dc3ce72f2d2d2aa1fbee73467fbfe1f6372e1
mikechao <mike.chao.one@gmail.com>
2025-02-24 22:34:53 -0800

    First explanation of how it works

--- pages/index.vue
+++ pages/index.vue
@@ -32,10 +32,27 @@
       <ULandingCard
         title="Phase 1: Generate Search Queries"
         description="The agent generates search queries based on the company name and the JSON schema provided by the user"
+        icon="i-mdi-numeric-1-circle-outline"
       />
       <ULandingCard
         title="Phase 2: Search the Web"
         description="The agent searches the web for relevant information using the generated search queries"
+        icon="i-mdi-numeric-2-circle-outline"
+      />
+      <ULandingCard
+        title="Phase 3: Extract Notes"
+        description="The agent extracts notes from the search results that match the JSON schema provided by the user"
+        icon="i-mdi-numeric-3-circle-outline"
+      />
+      <ULandingCard
+        title="Phase 4: Generate Report"
+        description="The agent generates a report containing the extracted notes and the search results"
+        icon="i-mdi-numeric-4-circle-outline"
+      />
+      <ULandingCard
+        title="Phase 5: Reflection"
+        description="The agent reviews on how well the generated report is filled out. If the agent is not satisfied, it will generate additional search queries and return to Phase 2"
+        icon="i-mdi-numeric-5-circle-outline"
       />
     </UPageGrid>
   </ULandingSection>
