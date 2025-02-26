41a09f2bb10a842499cca6a1d3d70932a7a1b40a
mikechao <mike.chao.one@gmail.com>
2025-02-26 14:22:40 -0800

    Wrap LandingSection How it works with div the it scrolls to the top

--- pages/index.vue
+++ pages/index.vue
@@ -21,60 +21,61 @@
       />
     </template>
   </ULandingHero>
-  <ULandingSection
-    id="how-it-works"
-    icon="i-mdi-head-cog-outline"
-    data-aos="fade-up"
-    data-aos-easing="ease-out-cubic"
-    data-aos-duration="1500"
-    title="How it works"
-    align="left"
-    :ui="{
-      features: {
-        description: 'whitespace-pre-line',
-      },
-    }"
-    :features="[
-      {
-        name: 'Generate Queries',
-        description: 'The agent generates search queries based on the company name and report schema provided.',
-        icon: 'i-mdi-numeric-1-circle-outline',
-      },
-      {
-        name: 'Research Company',
-        description: 'The agent executes the generated queries. Then the agent generates notes based on the search results and report schema.',
-        icon: 'i-mdi-numeric-2-circle-outline',
-      },
-      {
-        name: 'Gather Notes and Extract Schema',
-        description: 'The agent gathers the notes and fills out the report schema from the notes.',
-        icon: 'i-mdi-numeric-3-circle-outline',
-      },
-      {
-        name: 'Reflection',
-        description: 'The agent evaluates the quality of the report schema for the following:\nAnalyzes completeness of required fields\nIdentifies any missing or incomplete information\nGenerates targeted follow-up search queries if needed\nContinues research until information is satisfactory or max reflection steps reached',
-        icon: 'i-mdi-numeric-4-circle-outline',
-      },
-    ]"
-  >
-    <template #description>
-      <span>
-        The Company Researcher Agent utilizes a graph created with
-        <a
-          href="https://langchain-ai.github.io/langgraph/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-primary-500 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-300 transition-colors"
-        >
-          LangGraph
-          <UIcon name="i-mdi-launch" class="inline-block align-middle" />
-        </a>
-        to scan the web for details about a company provided by the user.
-      </span>
-    </template>
-    <img
-      src="/graph.png"
-      class="w-fit rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700 mx-auto"
+  <div id="how-it-works">
+    <ULandingSection
+      icon="i-mdi-head-cog-outline"
+      data-aos="fade-up"
+      data-aos-easing="ease-out-cubic"
+      data-aos-duration="1500"
+      title="How it works"
+      align="left"
+      :ui="{
+        features: {
+          description: 'whitespace-pre-line',
+        },
+      }"
+      :features="[
+        {
+          name: 'Generate Queries',
+          description: 'The agent generates search queries based on the company name and report schema provided.',
+          icon: 'i-mdi-numeric-1-circle-outline',
+        },
+        {
+          name: 'Research Company',
+          description: 'The agent executes the generated queries. Then the agent generates notes based on the search results and report schema.',
+          icon: 'i-mdi-numeric-2-circle-outline',
+        },
+        {
+          name: 'Gather Notes and Extract Schema',
+          description: 'The agent gathers the notes and fills out the report schema from the notes.',
+          icon: 'i-mdi-numeric-3-circle-outline',
+        },
+        {
+          name: 'Reflection',
+          description: 'The agent evaluates the quality of the report schema for the following:\nAnalyzes completeness of required fields\nIdentifies any missing or incomplete information\nGenerates targeted follow-up search queries if needed\nContinues research until information is satisfactory or max reflection steps reached',
+          icon: 'i-mdi-numeric-4-circle-outline',
+        },
+      ]"
     >
-  </ULandingSection>
+      <template #description>
+        <span>
+          The Company Researcher Agent utilizes a graph created with
+          <a
+            href="https://langchain-ai.github.io/langgraph/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-primary-500 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-300 transition-colors"
+          >
+            LangGraph
+            <UIcon name="i-mdi-launch" class="inline-block align-middle" />
+          </a>
+          to scan the web for details about a company provided by the user.
+        </span>
+      </template>
+      <img
+        src="/graph.png"
+        class="w-fit rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700 mx-auto"
+      >
+    </ULandingSection>
+  </div>
 </template>
