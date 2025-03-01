4820cc40be28990177159196fc3db47577014ec9
mikechao <mike.chao.one@gmail.com>
2025-02-28 21:19:38 -0800

    Create a header component

---
+++ components/AppHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <header
+    class="relative z-20 flex w-full items-center justify-between px-4 py-2"
+  >
+    <div class="size-8">
+      <NuxtLink to="/" class="flex items-center">
+        <img
+          src="/logo-dark.svg"
+          alt="Company Researcher Logo"
+          class="h-8 w-auto dark:hidden"
+        >
+        <img
+          src="/logo-light.svg"
+          alt="Company Researcher Logo"
+          class="h-8 w-auto hidden dark:block"
+        >
+      </NuxtLink>
+    </div>
+    <div
+      class="border-dark-200/60 bg-dark-50/30 dark:border-dark-500/5 dark:bg-dark-900/90 hidden items-center justify-center rounded-full border px-24 py-1 backdrop-blur md:flex"
+    >
+      <nav class="flex w-full items-center justify-center gap-2">
+        <UButton
+          variant="ghost"
+          color="primary"
+          size="lg"
+          to="/"
+          class="rounded-full px-4"
+        >
+          Home
+        </UButton>
+        <UButton
+          variant="ghost"
+          color="primary"
+          size="lg"
+          to="/research"
+          class="rounded-full px-4"
+        >
+          Research
+        </UButton>
+      </nav>
+    </div>
+    <div class="flex w-auto gap-4">
+      <ColorModeButton />
+      <USlideover title="Menu" close-icon="i-mdi-alpha-x">
+        <div class="inline-block md:hidden">
+          <UButton
+            icon="i-mdi-menu"
+            variant="link"
+            color="primary"
+            size="lg"
+          />
+        </div>
+        <template #body>
+          <nav class="flex w-full flex-col gap-2">
+            <UButton
+              variant="ghost"
+              color="primary"
+              size="lg"
+              to="/"
+              class="rounded-full px-4"
+            >
+              Home
+            </UButton>
+            <UButton
+              variant="ghost"
+              color="primary"
+              size="lg"
+              to="/research"
+              class="rounded-full px-4"
+            >
+              Research
+            </UButton>
+          </nav>
+        </template>
+      </USlideover>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,8 @@
-<script setup lang="ts">
-import ColorModeButton from '~/components/ColorModeButton.vue'
-
-const items = [
-  {
-    label: 'Home',
-    icon: 'i-mdi-home',
-    to: '/',
-  },
-  {
-    label: 'Research',
-    icon: 'i-mdi-microscope',
-    to: '/research',
-  },
-]
-</script>
-
 <template>
-  <div class="flex flex-col h-screen overflow-hidden">
-    <div class="flex items-center justify-between border-b border-gray-200 dark:border-gray-800">
-      <UNavigationMenu :items="items" />
-      <ColorScheme placeholder="..." tag="span" class="mr-2">
-        <ColorModeButton class="mr-2" />
-      </ColorScheme>
-    </div>
-    <div class="flex-1 overflow-auto bg-white dark:bg-gray-900">
+  <div class="relative w-full min-h-dvh h-full bg-dark-50 dark:bg-dark-950">
+    <AppHeader />
+    <UContainer class="max-w-[1680px]">
       <slot />
-    </div>
+    </UContainer>
   </div>
 </template>
