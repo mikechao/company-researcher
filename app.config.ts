4e5d116f026c75a4e3863bab79e0a21ddea96ce6
mikechao <mike.chao.one@gmail.com>
2025-03-01 13:40:26 -0800

    Use default colors

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,5 @@
 export default defineAppConfig({
   ui: {
-    colors: {
-      primary: 'green',
-      neutral: 'zinc',
-    },
     icons: {
       chevronDown: 'i-mdi-chevron-down-box-outline',
     },
