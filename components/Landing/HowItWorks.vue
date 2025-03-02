ac176cd5cccebb9cf482d67b295df4b5e6be71d9
mikechao <mike.chao.one@gmail.com>
2025-03-02 12:04:50 -0800

    Change up how it works section

--- components/Landing/HowItWorks.vue
+++ components/Landing/HowItWorks.vue
@@ -1,31 +1,89 @@
+<script setup lang="ts">
+const features = [
+  {
+    icon: 'i-mdi-numeric-1-circle-outline',
+    title: 'Generate Queries',
+    description:
+      'The agent generates search queries based on the company name and report schema provided.',
+    link: '#',
+  },
+  {
+    icon: 'i-mdi-numeric-2-circle-outline',
+    title: 'Research Company',
+    description:
+      'The agent executes the generated queries. Then the agent generates notes based on the search results and report schema.',
+    link: '#',
+  },
+  {
+    icon: 'i-mdi-numeric-3-circle-outline',
+    title: 'Gather Notes and Extract Schema',
+    description:
+      'The agent gathers the notes and fills out the report schema from the notes.',
+    link: '#',
+  },
+  {
+    icon: 'i-mdi-numeric-4-circle-outline',
+    title: 'Reflection',
+    description:
+      'The agent evaluates the quality of the report schema for the following:\nAnalyzes completeness of required fields\nIdentifies any missing or incomplete information\nGenerates targeted follow-up search queries if needed\nContinues research until information is satisfactory or max reflection steps reached',
+    link: '#',
+  },
+]
+</script>
+
 <template>
-  <div class="bg-dark-50 dark:bg-dark-950 relative h-auto w-full">
-    <div
-      class="2xl:max-w-screen-2xl mx-auto grid h-full min-h-dvh w-full grid-cols-2 items-center justify-center gap-4 px-4"
-    >
-      <div class="relative col-span-full mt-28 lg:col-span-1">
-        <div class="flex flex-col items-center justify-center lg:items-start">
-          <h1
-            class="text-dark-900 dark:text-dark-100 w-full text-center font-sans text-4xl leading-10 font-bold sm:text-5xl md:w-10/12 lg:w-full lg:text-left lg:text-6xl"
+  <div class="flex flex-col items-center justify-start gap-x-24 lg:flex-row">
+    <div class="flex w-full flex-col gap-y-24 lg:w-1/2">
+      <div class="flex flex-col gap-y-2">
+        <span
+          class="text-dark-950 dark:text-dark-50 inline-block text-3xl font-bold"
+        >How it works</span>
+        <p class="text-dark-950/50 dark:text-dark-50/50 text-lg font-normal">
+          The Company Researcher Agent utilizes a graph created with
+          <a
+            href="https://langchain-ai.github.io/langgraph/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-primary-500 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-300 transition-colors"
           >
-            How it works
-          </h1>
-          <p
-            class="text-primary-950/60 dark:text-primary-50/50 mt-4 w-full text-center text-lg leading-8 md:w-1/2 lg:w-3/5 lg:text-left 2xl:text-xl"
-          >
-            Unlock your construction project's full potential with our
-            comprehensive suite of tools and services
-          </p>
-        </div>
+            LangGraph
+            <UIcon name="i-mdi-launch" class="inline-block align-middle" />
+          </a>
+          to scan the web for details about a company provided by the user.
+        </p>
       </div>
-      <div class="relative col-span-full mt-4 lg:col-span-1">
-        <NuxtImg
-          class="max-h-[420px] w-full rounded-lg object-contain object-center lg:max-h-[720px]"
-          src="/graph.png"
-          loading="lazy"
-          alt="langgraph-node-graph"
-        />
+      <div class="grid w-full grid-cols-4 gap-24">
+        <template v-for="(feat, index) in features" :key="index">
+          <div class="col-span-full flex flex-col gap-y-2 md:col-span-2">
+            <div
+              class="border-dark-950/20 bg-dark-200/20 dark:border-dark-700 dark:bg-dark-800 flex size-8 items-center justify-center rounded-md border p-1"
+            >
+              <UIcon
+                :name="feat.icon"
+                class="text-dark-950 dark:text-dark-50"
+              />
+            </div>
+            <span
+              class="text-dark-950 dark:text-dark-50 inline-block text-lg font-bold"
+            >{{ feat.title }}</span>
+            <p
+              class="text-dark-950/50 dark:text-dark-50 inline-block text-base font-normal whitespace-pre-line"
+            >
+              {{ feat.description }}
+            </p>
+          </div>
+        </template>
       </div>
+    </div>
+    <div class="relative mt-24 h-full w-full lg:mt-0 lg:w-1/2 xl:h-[620px]">
+      <img
+        src="/graph.png"
+        class="max-h-full max-w-full rounded-sm object-contain"
+        loading="lazy"
+        alt="langgraph-graph"
+        width="640"
+        height="640"
+      >
     </div>
   </div>
 </template>
