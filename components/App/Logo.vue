f86b6b1fdcef520a36ac93928695d959a6996a04
mikechao <mike.chao.one@gmail.com>
2025-03-03 14:37:29 -0800

    Set width and height for logo

--- components/App/Logo.vue
+++ components/App/Logo.vue
@@ -3,10 +3,14 @@
     src="/logo-dark.svg"
     alt="Company Researcher Logo"
     class="h-8 w-auto dark:hidden"
+    width="32"
+    height="32"
   >
   <img
     src="/logo-light.svg"
     alt="Company Researcher Logo"
     class="h-8 w-auto hidden dark:block"
+    width="32"
+    height="32"
   >
 </template>
