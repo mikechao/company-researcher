ddf3f8a6b847bdb00d692c1353ad2dc9368f72ff
mikechao <mike.chao.one@gmail.com>
2025-03-03 14:05:38 -0800

    Set favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 import LandingHero from '~/components/Landing/Hero.vue'
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+  ],
+})
 </script>
 
 <template>
