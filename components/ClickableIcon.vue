e73ff3f1f0c79589c3355dfb400ca29587fe3988
mikechao <mike.chao.one@gmail.com>
2025-03-05 15:19:28 -0800

    Added tooltip to show href in marquee

--- components/ClickableIcon.vue
+++ components/ClickableIcon.vue
@@ -17,14 +17,16 @@ defineProps({
 
 <template>
   <div class="size-auto items-center justify-center p-2">
-    <a
-      :href="href"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="border-dark-950/20 bg-dark-200/20 dark:border-dark-700 dark:bg-dark-800 flex size-16 items-center justify-center rounded-md border p-1"
-      :aria-label="label"
-    >
-      <UIcon :name="icon" class="size-16 text-dark-500/50 dark:text-dark-50/20" />
-    </a>
+    <UTooltip :delay-duration="0" arrow :text="href">
+      <a
+        :href="href"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="border-dark-950/20 bg-dark-200/20 dark:border-dark-700 dark:bg-dark-800 flex size-16 items-center justify-center rounded-md border p-1"
+        :aria-label="label"
+      >
+        <UIcon :name="icon" class="size-16 text-dark-500/50 dark:text-dark-50/20" />
+      </a>
+    </UTooltip>
   </div>
 </template>
