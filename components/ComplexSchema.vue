9cf3481eb42b32f1bc73869fe222bd3d2d77e06e
mikechao <mike.chao.one@gmail.com>
2025-03-10 15:31:11 -0700

    Dynamically load the complex schema/NuxtCodeMirror

---
+++ components/ComplexSchema.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { jsonLanguage } from '@codemirror/lang-json'
+import { okaidia } from '@uiw/codemirror-theme-okaidia'
+import { complexSchema } from '~/types/constants'
+
+const extensions = [jsonLanguage]
+const complexSchemaJSON = JSON.stringify(complexSchema, null, 2)
+</script>
+
+<template>
+  <NuxtCodeMirror
+    v-model="complexSchemaJSON"
+    :extensions="extensions"
+    :theme="okaidia"
+    :auto-focus="true"
+    :editable="false"
+    :basic-setup="true"
+    :indent-with-tab="true"
+    class="w-full h-full overflow-auto"
+  />
+</template>
--- pages/complex.vue
+++ pages/complex.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import { jsonLanguage } from '@codemirror/lang-json'
-import { okaidia } from '@uiw/codemirror-theme-okaidia'
-import CopyButton from '~/components/results/CopyButton.vue'
 import { complexSchema } from '~/types/constants'
 
-const extensions = [jsonLanguage]
-const complexSchemaJSON = JSON.stringify(complexSchema, null, 2)
+const ComplexSchema = defineAsyncComponent(() => import('~/components/ComplexSchema.vue'))
+const CopyButton = defineAsyncComponent(() => import('~/components/results/CopyButton.vue'))
 </script>
 
 <template>
@@ -19,16 +16,7 @@ const complexSchemaJSON = JSON.stringify(complexSchema, null, 2)
     </div>
     <p>Below is an example of a more complex report schema which will result in longer execution times, but provide more information.</p>
     <div class="w-full mt-1 flex-1 overflow-auto">
-      <NuxtCodeMirror
-        v-model="complexSchemaJSON"
-        :extensions="extensions"
-        :theme="okaidia"
-        :auto-focus="true"
-        :editable="false"
-        :basic-setup="true"
-        :indent-with-tab="true"
-        class="w-full h-full overflow-auto"
-      />
+      <ComplexSchema />
     </div>
   </div>
 </template>
